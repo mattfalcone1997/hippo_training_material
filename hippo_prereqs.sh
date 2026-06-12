872302f5ab4bde1a1d6dfe68c1457cbed2892a46
Matthew Falcone <mapfalcone@gmail.com>
2026-06-12 13:33:21 +0100

    Slight update to prereq

--- hippo_prereqs.sh
+++ hippo_prereqs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export PATH=/opt/hippo:$PATH
-export PATH=/opt/moose/modules/thermal_hydraulics:$PATH
-export MOOSE_LIBRARY_PATH=/opt/hippo/lib
-unset FOAM_SIGFPE
-
 set_nprocs(){
     # Set number of processes to be run ion
     if [[ $# -eq 1 ]]; then
